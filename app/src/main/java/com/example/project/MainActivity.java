8db2c677323871f129e7bd785c5d8110b57b33b4
jinyeob <jinyeob07@gmail.com>
2019-05-30 16:51:41 +0900

    ver5 동그라미 여러개 가능하게 했음(배열사용)

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -8,6 +8,7 @@ import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Point;
+import android.graphics.Typeface;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,22 +17,21 @@ import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
     protected class MyView extends View {
-        int x = 100, y = 100;
+        int j=0;
+        int[] x2=new int[] {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+        int[] y2=new int[] {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+        public int cntCrt=0, cntWrg=0;
         public MyView(Context context) {
             super(context);
-            setup();
         }
         private Paint paint = new Paint();
 
-        private void setup(){
-            paint.setStyle(Paint.Style.STROKE);
-            paint.setStrokeWidth(10);
-            paint.setColor(Color.RED);
-        }
         @Override
         protected void onDraw(Canvas canvas) {
 
@@ -43,32 +43,49 @@ public class MainActivity extends AppCompatActivity {
 
             canvas.drawBitmap(resize_bitmap, 0, 0, paint);
 
-            canvas.drawCircle(x, y, 30, paint);
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeWidth(10);
+            paint.setColor(Color.RED);
+
+            for(int i=0;i<j;i++){
+                canvas.drawCircle(x2[i], y2[i], 30, paint);
+            }
+
+            paint.setAntiAlias(true);
+            paint.setColor(Color.BLACK);
+            paint.setTextSize(50);
+            paint.setStrokeWidth(1);
+
+            canvas.drawText("맞은개수: "+cntCrt, 100, 50, paint);
+            canvas.drawText("틀린개수: "+cntWrg, 450, 50, paint);
+//            canvas2=canvas;
         }
         @Override
         public boolean onTouchEvent(MotionEvent event) {
             switch(event.getAction()) {
                 case MotionEvent.ACTION_DOWN:
-                    x = (int) event.getX();
-                    y = (int) event.getY();
+                    x2[j] = (int) event.getX();
+                    y2[j] = (int) event.getY();
                     //x 330~400
                     //y 150~230
-                    if ((330 < x && x < 400) && (150 < y && y < 230)) {
-                        Toast.makeText(MainActivity.this, "정답입니다!", Toast.LENGTH_SHORT).show();
-                        invalidate();
-
+                    if ((330 < x2[j] && x2[j] < 400) && (150 < y2[j] && y2[j] < 230)) {
+                            Toast.makeText(MainActivity.this, "정답입니다!", Toast.LENGTH_SHORT).show();
+                            cntCrt++;
+                            invalidate();
                     } else {
                         Toast.makeText(MainActivity.this, "틀렸습니다!", Toast.LENGTH_SHORT).show();
+                        cntWrg++;
+                       invalidate();
                     }
+                    j++;
 //                    String msg = "터치를 입력받음 : " +x+" / " +y;
 //                    Toast. makeText(MainActivity. this, msg, Toast.LENGTH_SHORT ).show();
                 case MotionEvent.ACTION_MOVE:
                 case MotionEvent.ACTION_UP:
-
             }
-
             return true;
         }
+
     }
 
         @Override
@@ -76,6 +93,7 @@ public class MainActivity extends AppCompatActivity {
             super.onCreate(savedInstanceState);
             MyView w = new MyView(this);
             setContentView(w);
+
         }
 
 }
